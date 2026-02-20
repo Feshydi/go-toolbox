12056d1de852d2590af78f4324a5edc2f27694dd
Bogdan Iushchenko <the727dimension@gmail.com>
2026-02-20 13:08:26 +0200

    add shutdown waiter

---
+++ pkg/shutdown/shutdown.go
@@ -0,0 +1,13 @@
+package shutdown
+
+import (
+	"os"
+	"os/signal"
+	"syscall"
+)
+
+func Wait() os.Signal {
+	ch := make(chan os.Signal, 1)
+	signal.Notify(ch, syscall.SIGINT, syscall.SIGTERM)
+	return <-ch
+}
